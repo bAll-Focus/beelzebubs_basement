3dbb65245365216a3a43c435f46fa56f81de4ce3
Marcus Erlandsson <marcus.erlandsson99@gmail.com>
2025-11-25 15:21:00 +0100

    Fix issue with the right hand listening to the left hand

--- src/beelzebub's-basement/Scripts/spell_detector.gd
+++ src/beelzebub's-basement/Scripts/spell_detector.gd
@@ -6,6 +6,14 @@ var _making_sign := false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_parent_controller = get_parent() as XRController3D
+	if _parent_controller:
+		var hand = "unknown"
+		if _parent_controller.tracker:
+			if "/left" in _parent_controller.tracker:
+				hand = "left"
+			elif "/right" in _parent_controller.tracker:
+				hand = "right"
+		print("SpellDetector initialized for ", hand, " hand (tracker: ", _parent_controller.tracker, ")")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,4 +25,10 @@ func _process(delta: float) -> void:
 	_making_sign = _parent_controller.is_button_pressed("thumbs_up")
 	if _making_sign and not old_sign_made:
 		# New pose detected
-		print("Thumbs up detected")
+		var hand = "unknown"
+		if _parent_controller.tracker:
+			if "/left" in _parent_controller.tracker:
+				hand = "left"
+			elif "/right" in _parent_controller.tracker:
+				hand = "right"
+		print("Thumbs up detected on ", hand, " hand (tracker: ", _parent_controller.tracker, ")")
