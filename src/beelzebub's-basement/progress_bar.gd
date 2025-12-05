5bae9eeabd3bcc11171ccd4a1e1e2c8ddada06aa
Rej Karlander <74512132+Fetaost-code@users.noreply.github.com>
2025-12-05 20:14:03 +0100

    Squashed commit of the following:
    commit 2181555e6fe249a13dacc26de993ef6b712bf687
    Merge: 72dfcdc c50ea39
    Author: Rej Karlander <74512132+Fetaost-code@users.noreply.github.com>
    Date:   Fri Dec 5 20:00:55 2025 +0100
    
        Merge branch 'main' into 10-system-for-spells
    
    commit 72dfcdc17375069c01e98cc74f5aec0448a9ce39
    Author: Rej Karlander <74512132+Fetaost-code@users.noreply.github.com>
    Date:   Fri Dec 5 19:54:18 2025 +0100
    
        functional spells in multiplayer
    
    commit 472a7175fef00d0e3ac900efcfbb65bbe8f6b98a
    Author: Rej Karlander <74512132+Fetaost-code@users.noreply.github.com>
    Date:   Fri Dec 5 19:02:43 2025 +0100
    
        wip merger
    
    commit 6d4f9608d0a85c6965fc743aa1b1f65d3034b9c3
    Author: Jennifer Köhler <89851567+JenKohKTH@users.noreply.github.com>
    Date:   Fri Dec 5 13:58:49 2025 +0100
    
        Merge merged, issues I think solved
    
    commit d14932f4558f7898c6f4aea9cd8ea602eb443db6
    Merge: f1d93c9 bbe414e
    Author: Jennifer Köhler <89851567+JenKohKTH@users.noreply.github.com>
    Date:   Fri Dec 5 13:49:43 2025 +0100
    
        Merge branch 'main' into 10-system-for-spells
    
    commit f1d93c9c1a199cc89cfa3959b2343bad08125d01
    Author: Jennifer KÃ¶hler <jennifer.kohler02@gmail.se>
    Date:   Thu Dec 4 21:40:40 2025 +0100
    
        Add models for basement furniture
    
    commit 90c866e7bfe2a7144db99c8a313a9d8180272ffd
    Author: Jennifer KÃ¶hler <jennifer.kohler02@gmail.se>
    Date:   Thu Dec 4 12:49:44 2025 +0100
    
        Add colours to healthbar decrease based on attack type
    
    commit 5c0469e61896fe061c638eaa8c96f0952ed866cc
    Author: Jennifer KÃ¶hler <jennifer.kohler02@gmail.se>
    Date:   Wed Dec 3 14:54:26 2025 +0100
    
        Add spell effects to Ba'al from (keyboard input)

--- src/beelzebub's-basement/progress_bar.gd
+++ src/beelzebub's-basement/progress_bar.gd
@@ -2,6 +2,9 @@ extends ProgressBar
 
 @onready var timer = $Timer
 
+var fireColour = Color(0.902, 0.459, 0.184, 1.0)
+var iceColour = Color(0.490, 0.765, 0.953, 1.0)
+var cabbageColour = Color(0.553, 0.835, 0.561, 1.0)
 var health = 0 : set = _set_health
 
 func _init_health(_health):
@@ -32,3 +35,11 @@ func _reset():
 	health = max_value
 	value = health
 	$Damagebar.value = health
+
+func _set_colour(tempVal):
+	if tempVal == 1:
+		$Damagebar.get("theme_override_styles/fill").bg_color = iceColour
+	elif tempVal == 2:
+		$Damagebar.get("theme_override_styles/fill").bg_color = fireColour
+	else: 
+		$Damagebar.get("theme_override_styles/fill").bg_color = cabbageColour
