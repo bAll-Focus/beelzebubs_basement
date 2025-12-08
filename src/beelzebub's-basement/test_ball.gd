fe86e716a500fc23a44321a45bf12af90b4ca31e
Kei <keidukebergman@gmail.com>
2025-12-08 19:02:30 +0100

    remove old code

--- src/beelzebub's-basement/test_ball.gd
+++ src/beelzebub's-basement/test_ball.gd
@@ -16,7 +16,6 @@ var damage = DAMAGES[damageIndex]
 
 func _ready() -> void:
 	set_damage_type(0)
-	
 
 func set_damage_type(damage_type):
 	if(multiplayer.is_server()):
@@ -45,28 +44,3 @@ func _physics_process(delta: float) -> void:
 func _collide():
 	set_damage_type(0)
 	print("Ball: Collided with demon")
-
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	#var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	#var input_vertical := Input.get_axis("down", "up")
-	#var vertical := (transform.basis * Vector3(0, input_vertical, 0))
-	#
-	#if direction:
-		#velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
-		#
-	#if vertical:
-		#velocity.y = vertical.y * SPEED
-	#else:
-		#velocity.y = move_toward(velocity.y, 0, SPEED)
-#
-	#move_and_slide()
