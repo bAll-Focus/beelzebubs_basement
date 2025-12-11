9366af5605b669679c1d54744c78d7a12d52b813
Kei <keidukebergman@gmail.com>
2025-12-11 16:14:23 +0100

    add bertbody

--- src/beelzebub's-basement/Scenes/tentacle.gd
+++ src/beelzebub's-basement/Scenes/tentacle.gd
@@ -2,13 +2,14 @@ extends Node3D
 
 @export var end_position:Vector3
 @export var movedistance:float
+@export var move_direction:Vector3
 var start_position:Vector3
 @export var movement_speed:float
 var to_be_shown:bool = false
 
 
 func _ready():
-	position = end_position - movedistance * basis.z
+	position = end_position - movedistance * move_direction
 	start_position = position
 	to_be_shown = true
 
